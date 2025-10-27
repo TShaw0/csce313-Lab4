6f05613b5c66260efa4237b5c4f6e69d30973edc
TShaw0 <shaw.trenton06@gmail.com>
2025-10-27 20:31:52 +0000

    100 and comments

--- MasterChef.cpp
+++ MasterChef.cpp
@@ -12,178 +12,146 @@
 
 using namespace std;
 
-StepList* recipeSteps;
-vector<int>* completedSteps;
-int completeCount = 0;
+// Global variables
+StepList* recipeSteps;          // Pointer to the list of all recipe steps
+vector<int>* completedSteps;    // Stores IDs of steps that have completed
+int completeCount = 0;          // Total number of completed steps
 
-void PrintHelp() // Given
-{
-
-  cout << "Usage: ./MasterChef -i <file>\n\n";
-  cout<<"--------------------------------------------------------------------------\n";
-  cout<<"<file>:    "<<"csv file with Step, Dependencies, Time (m), Description\n";
-  cout<<"--------------------------------------------------------------------------\n";
-  exit(1);
+// Prints usage information and exits the program
+void PrintHelp() {
+    cout << "Usage: ./MasterChef -i <file>\n\n";
+    cout << "--------------------------------------------------------------------------\n";
+    cout << "<file>:    csv file with Step, Dependencies, Time (m), Description\n";
+    cout << "--------------------------------------------------------------------------\n";
+    exit(1);
 }
 
-string ProcessArgs(int argc, char** argv) // Given
-{
-  string result = "";
-  // print help if odd number of args are provided
-  if (argc < 3) {
-    PrintHelp();
-  }
-  
-  while (true)
-    {
-      const auto opt = getopt(argc, argv, "i:h");
-      
-      if (-1 == opt)
-	break;
-      
-      switch (opt)
-	{
-	case 'i':
-	  result = std::string(optarg);
-	  break;
-	case 'h': // -h or --help
-	default:
-	  PrintHelp();
-	  break;
-	}
+// Processes command line arguments and returns the input filename
+string ProcessArgs(int argc, char** argv) {
+    string result = "";
+
+    // If fewer than expected arguments, show help
+    if (argc < 3) {
+        PrintHelp();
     }
-  
-  return result;
+
+    while (true) {
+        const auto opt = getopt(argc, argv, "i:h");
+
+        if (-1 == opt) break;  // No more options
+
+        switch (opt) {
+            case 'i':  // Input file
+                result = string(optarg);
+                break;
+            case 'h':  // Help option
+            default:
+                PrintHelp();
+                break;
+        }
+    }
+
+    return result;
 }
 
-// Creates and starts a timer given a pointer to the step to start and when it will expire in seconds.
-void makeTimer( Step *timerID, int expire) // Given
-{
-  struct sigevent te;
-  struct itimerspec its;
-  
-  /* Set and enable alarm */
-  te.sigev_notify = SIGEV_SIGNAL;
-  te.sigev_signo = SIGRTMIN;
-  te.sigev_value.sival_ptr = timerID;
-  timer_create(CLOCK_REALTIME, &te, &(timerID->t_id));
-  
-  its.it_interval.tv_sec = 0;
-  its.it_interval.tv_nsec = 0;
-  its.it_value.tv_sec = expire;
-  its.it_value.tv_nsec = 0;
-  timer_settime(timerID->t_id, 0, &its, NULL);
+// Creates and starts a timer for a given step
+// expire is the number of seconds until the step completes
+void makeTimer(Step* timerID, int expire) {
+    struct sigevent te;
+    struct itimerspec its;
+
+    // Set up timer to send SIGRTMIN to this process when expired
+    te.sigev_notify = SIGEV_SIGNAL;
+    te.sigev_signo = SIGRTMIN;
+    te.sigev_value.sival_ptr = timerID;  // Pass the step pointer to the handler
+    timer_create(CLOCK_REALTIME, &te, &(timerID->t_id));
+
+    // Configure the timer: single-shot (no interval), expires after "expire" seconds
+    its.it_interval.tv_sec = 0;
+    its.it_interval.tv_nsec = 0;
+    its.it_value.tv_sec = expire;
+    its.it_value.tv_nsec = 0;
+    timer_settime(timerID->t_id, 0, &its, NULL);
 }
 
-// Triggers when the time for the step has elapsed.
-// the siginfo_t* pointer holds the step information in the si_value.sival_ptr
-// You will need to print out that the step has completed
-// Mark the step to be removed as a dependency, and trigger the remove dep handler.
-// TO COMPLETE Section 2
-static void timerHandler( int sig, siginfo_t *si, void *uc )
-{
-  // Retrieve timer pointer from the si->si_value
-  Step* comp_item = (Step*)si->si_value.sival_ptr;
-  
-  /* TODO This Section - 2 */
-  // Officially complete the step using completedSteps and completeCount
-  
-  if (comp_item != nullptr) {
+// Signal handler for when a step's timer expires
+static void timerHandler(int sig, siginfo_t* si, void* uc) {
+    Step* comp_item = (Step*)si->si_value.sival_ptr; // Retrieve pointer to the step
 
-    completedSteps->push_back(comp_item->id); // mark completed first
-    completeCount++;
-    comp_item->PrintComplete();                 // then print completion
-    raise(SIGUSR1);                             // then remove dependencies
+    if (comp_item != nullptr) {
+        // Mark the step as completed
+        completedSteps->push_back(comp_item->id);  // Add step ID to completed list
+        completeCount++;                            // Increment total completed count
 
-  }
-  
-  /* End Section - 2 */
+        comp_item->PrintComplete();                 // Print "Completed Step: ..."
+        
+        raise(SIGUSR1);                             // Notify dependency handler to update steps
+    }
 }
 
-// Removes the copmleted steps from the dependency list of the step list.
-// Utilize the completedSteps vector and the RemoveDependency method.
-// To Complete - Section 3
+// Signal handler for removing completed steps from other steps' dependency lists
 void RemoveDepHandler(int sig) {
-  /* TODO This Section - 3 */
-  // Foreach step that has been completed since last run, remove it as a dependency
-  
-  if (completedSteps == nullptr || recipeSteps == nullptr) return;
-  
-  for (int id : *completedSteps) {
-    recipeSteps->RemoveDependency(id);
-  }
-  
-  // Clear completedSteps for next round
-  completedSteps->clear();
-  
-  /* End Section - 3 */
+    if (completedSteps == nullptr || recipeSteps == nullptr) return;
+
+    // Remove each completed step from dependencies of all steps
+    for (int id : *completedSteps) {
+        recipeSteps->RemoveDependency(id);
+    }
+
+    // Clear the completed steps list for next batch
+    completedSteps->clear();
 }
 
-// Associate the signals to the signal handlers as appropriate
-// Continuously check what steps are ready to be run, and start timers for them with makeTimer()
-// run until all steps are done.
-// To Complete - Section 1
-
-int main(int argc, char **argv)
-{
-  string input_file = ProcessArgs(argc, argv);
-  if(input_file.empty()) {
-    exit(1);
-  }
-  
-  // Initialize global variables
-  completedSteps = new vector<int>();
-  recipeSteps = new StepList(input_file);
-  
-  /* Set up signal handler. */
-  struct sigaction sa;
-  sa.sa_flags = SA_SIGINFO;
-  sa.sa_sigaction = timerHandler;
-  sigemptyset(&sa.sa_mask);
-  
-  /* TODO This Section - 1 */
-  // Associate the signal SIGRTMIN with the sa using the sigaction function
-  // Associate the appropriate handler with the SIGUSR1 signal, for removing dependencies
-  
-  if (sigaction(SIGRTMIN, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
-  
-  // Use the simple signal() API for SIGUSR1 as requested by the starter
-  if (signal(SIGUSR1, RemoveDepHandler) == SIG_ERR) {
-    perror("signal");
-    exit(1);
-  }
-  
-  // Until all steps have been completed, check if steps are ready to be run and create a timer for them if so
-  
-  int totalSteps = recipeSteps->Count();
-  
-  while (completeCount < totalSteps) {
-    // Query ready steps
-    vector<Step*> ready = recipeSteps->GetReadySteps();
-    sort(ready.begin(), ready.end(), [](Step* a, Step* b){ return a->id < b->id; });
-
-    // Start timers for ready steps (don't start if already running)
-    for (Step* s : ready) {
-      if (!s->running) {
-	s->running = true;
-	makeTimer(s, s->duration);
-      }
+int main(int argc, char **argv) {
+    string input_file = ProcessArgs(argc, argv);
+    if (input_file.empty()) {
+        exit(1);
     }
-    // Sleep briefly to avoid busy spinning and allow signals to be delivered
-    usleep(100 * 1000); 
-  }
 
-  /* End Section - 1 */
+    // Initialize global structures
+    completedSteps = new vector<int>();
+    recipeSteps = new StepList(input_file);
 
-  cout << "Enjoy!" << endl;
+    // Set up signal handler for SIGRTMIN (timers)
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = timerHandler;
+    sigemptyset(&sa.sa_mask);
 
-  // cleanup
-  
-  //  delete recipeSteps;
-  //delete completedSteps;
-  
-  return 0;
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    // Set up signal handler for SIGUSR1 (removing dependencies)
+    if (signal(SIGUSR1, RemoveDepHandler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+
+    int totalSteps = recipeSteps->Count();
+
+    // Main loop: repeatedly check for steps ready to run
+    while (completeCount < totalSteps) {
+        // Retrieve all steps that are ready (no dependencies and not running)
+        vector<Step*> ready = recipeSteps->GetReadySteps();
+
+        // Sort steps by ID to ensure deterministic output order
+        sort(ready.begin(), ready.end(), [](Step* a, Step* b){ return a->id < b->id; });
+
+        // Start timers for ready steps
+        for (Step* s : ready) {
+            if (!s->running) {
+                s->running = true;
+                makeTimer(s, s->duration);
+            }
+        }
+
+        // Sleep briefly to allow signals to be delivered
+        usleep(100 * 1000); // 100ms
+    }
+
+    cout << "Enjoy!" << endl;
+
+    return 0;
 }
